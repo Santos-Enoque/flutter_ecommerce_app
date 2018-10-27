81970d49c758d5eb3e00a82ffa43e407d5ebe37d
Santos Enoque <santos@Apples-MacBook-Pro-2.local>
2018-10-27 20:56:00 +0530

    recent products commit

--- lib/componets/products.dart
+++ lib/componets/products.dart
@@ -20,9 +20,21 @@ class _ProductsState extends State<Products> {
       "price": 50,
     }
   ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.builder(
+        itemCount: product_list.length,
+        gridDelegate:
+        new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return Single_prod(
+            prod_name: product_list[index]['name'],
+            prod_pricture: product_list[index]['picture'],
+            prod_old_price: product_list[index]['old_price'],
+            prod_price: product_list[index]['price'],
+          );
+        });
   }
 }
 
@@ -37,10 +49,45 @@ class Single_prod extends StatelessWidget {
     this.prod_pricture,
     this.prod_old_price,
     this.prod_price,
-});
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: Hero(
+          tag: prod_name,
+          child: Material(
+            child: InkWell(
+              onTap: () {},
+              child: GridTile(
+                  footer: Container(
+                    color: Colors.white70,
+                    child: ListTile(
+                        leading: Text(
+                          prod_name,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        title: Text(
+                          "\$$prod_price",
+                          style: TextStyle(
+                              color: Colors.red, fontWeight: FontWeight.w800),
+                        ),
+                        subtitle: Text(
+                          "\$$prod_old_price",
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w800,
+                              decoration
+                                  :TextDecoration.lineThrough),
+                        ),
+                    ),
+                  ),
+                  child: Image.asset(
+                    prod_pricture,
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          )),
+    );
   }
 }
-
